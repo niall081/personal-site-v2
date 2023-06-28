162e966c984cd9f161604b0135c3dc2c4a395ee2
Niall <niall081@gmail.com>
2023-06-28 16:16:44 +0800

    start publications page

---
+++ src/components/Article.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import '../css/main.css'
+import { FiMail } from 'react-icons/fi'
+import { GatsbyImage, StaticImage,getImage } from "gatsby-plugin-image"
+
+
+const Article = (props) => {
+    return (
+  ) 
+}
+
+export default Article
--- src/pages/publications.tsx
+++ src/pages/publications.tsx
@@ -1,17 +1,61 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
-import { Link } from "gatsby"
 import Navbar  from "../components/Navbar"
-
+import Footer from "../components/Footer"
+import Article from "../components/Article"
+import { StaticImage, getImage } from "gatsby-plugin-image"
+import { FaGithub, FaBook, FaFilePdf} from 'react-icons/fa'
 
 const PublicationsPage: React.FC<PageProps> = () => {
   return (
-    <main >
+    <div className="parent">
       <Navbar />
-      <div className="main">
-        <h1>This is the publications page</h1>
-      </div>
-    </main>
+      <main className="main">
+        <h1 className="section_title">Publications</h1>
+          <div id="journal-articles">
+            <h2 className="section_title--secondary"><span className="list_number--big">01.</span>Journal Articles</h2>
+
+            {/* First one */}
+            <div className="grid row">
+            <StaticImage 
+                  src="../images/icon.png" 
+                  alt="My picture" 
+                  className="row_img" 
+                />
+            <div>
+                <h3 className="row_text--title">"Random matrix theory of polarized light scattering in disordered media"</h3>
+                <p className="row_text">"N. Byrnes and M. R. Foreman, Waves Random Complex Media, in press (2022)"</p>
+            </div>
+            <div className="card_socials">
+                <a className="card_socials_icon" href="https://www.tandfonline.com/doi/full/10.1080/17455030.2022.2153305"><FaBook /></a>
+                <a className="card_socials_icon" href="../downloads/articles/article004.pdf"><FaFilePdf /></a>
+                <a className="card_socials_icon" href="https://github.com/niall081/random-matrix-polarized-scattering"><FaGithub /></a>
+            </div>
+            </div>
+
+            {/* Second one */}
+            <div className="grid row">
+            <StaticImage 
+                  src="../images/icon.png" 
+                  alt="My picture" 
+                  className="row_img" 
+                />
+            <div>
+                <h3 className="row_text--title">"Random matrix theory of polarized light scattering in disordered media"</h3>
+                <p className="row_text">"N. Byrnes and M. R. Foreman, Waves Random Complex Media, in press (2022)"</p>
+            </div>
+            <div className="card_socials">
+                <a className="card_socials_icon" href="https://www.tandfonline.com/doi/full/10.1080/17455030.2022.2153305"><FaBook /></a>
+                <a className="card_socials_icon" href="../downloads/articles/article004.pdf"><FaFilePdf /></a>
+                <a className="card_socials_icon" href="https://github.com/niall081/random-matrix-polarized-scattering"><FaGithub /></a>
+            </div>
+            </div>
+
+
+          </div>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
